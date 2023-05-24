1d2df6d5a674d65a44dbe9305734017b66554d1f
ckennx <ckennx@gmail.com>
2023-05-24 00:25:52 -0500

    initial commit

---
+++ src/components/TaskItem.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Button, Checkbox } from 'flowbite-svelte';
+
+	export let taskId = 0;
+	export let taskDescription = 'No description available';
+	export let completed = false;
+</script>
+
+<!-- svelte-ignore a11y-invalid-attribute -->
+<a
+	id="taskItem_{taskId}"
+	href="javascript:void(0);"
+	aria-label="Task item (#{taskId})"
+	class="me-3 bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 shadow-md flex flex-col hover:bg-gray-100 dark:hover:bg-gray-700 p-4 sm:p-6 max-w-full"
+>
+	<div class="flex items-center gap-4 p-2 text-lg">
+		<Checkbox checked={completed} class="w-6 h-6" />
+		<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">
+			{taskDescription}
+		</p>
+		<div class="flex align-center gap-3 ms-auto">
+			<Button outline color="light"><span class="material-icons">edit</span></Button>
+			<Button outline color="green" class="hidden"><span class="material-icons">check</span></Button
+			>
+			<Button color="red"><span class="material-icons">close</span></Button>
+		</div>
+	</div>
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import { Label, Textarea, Card, Button, Checkbox } from 'flowbite-svelte';
+	import TaskItem from '../components/TaskItem.svelte';
+
+	const taskItems = [
+		{
+			id: 0,
+			taskId: 0,
+			description: 'Do something 0',
+			completed: false
+		},
+		{
+			id: 1,
+			taskId: 0,
+			description: 'Do something 1',
+			completed: true
+		},
+		{
+			id: 2,
+			taskId: 0,
+			description: 'Do something 2',
+			completed: false
+		},
+		{
+			id: 3,
+			taskId: 0,
+			description: 'Do something 3',
+			completed: false
+		},
+		{
+			id: 4,
+			taskId: 0,
+			description: 'Do something 4',
+			completed: true
+		},
+		{
+			id: 5,
+			taskId: 0,
+			description: 'Do something 74',
+			completed: false
+		},
+		{
+			id: 75,
+			taskId: 0,
+			description: 'Do something 74',
+			completed: false
+		}
+	];
+
+	const task = {
+		id: 0,
+		userId: 0,
+		title: 'Task title',
+		description: 'Task description',
+		completed: false
+	};
+</script>
+
+<div class="container mx-auto flex gap-6 my-6">
+	<div class="grow w-1/3">
+		<h5 class="mb-6 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			Ticket title
+		</h5>
+		<div>
+			<Label for="first_name" class="hidden">First name</Label>
+			<Textarea type="text" id="first_name" placeholder="Content" required class="mb-3" rows="34" />
+		</div>
+	</div>
+	<div class="grow w-2/3 flex flex-col">
+		<div class="flex flex-col gap-6 overflow-y-scroll my-6 mt-14" style="max-height: 630px">
+			{#each taskItems as taskItem (taskItem.id)}
+				<TaskItem
+					taskId={taskItem.id}
+					taskDescription={taskItem.description}
+					completed={taskItem.completed}
+				/>
+			{/each}
+		</div>
+		<Button outline color="blue">
+			<span class="material-icons">add</span>
+			<span class="ms-2">Add task</span>
+		</Button>
+	</div>
+</div>
