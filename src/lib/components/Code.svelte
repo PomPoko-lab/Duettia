2feafaf31fe5e4999b865a77ab50ae87d20c5986
ckennx <ckennx@gmail.com>
2023-07-04 13:04:08 -0500

    JSON code snippet

---
+++ src/lib/components/Code.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let jsonData: unknown;
+
+	export let preClass: string = '';
+</script>
+
+<pre class="bg-gray-800 text-gray-200 rounded p-4 {preClass}">
+    {JSON.stringify(jsonData, null, 2)}
+</pre>
