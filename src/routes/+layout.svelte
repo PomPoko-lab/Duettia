b1a73b1f15047998ba0264704327a6d3d2c7903a
ckennx <ckennx@gmail.com>
2023-07-04 00:03:01 -0500

    responsive sidebar -- no mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div class="grid grid-cols-12 grid-rows-12 p-6 gap-4 h-screen">
-	<SideBar asideClass="col-span-2 row-span-11" />
-	<main class="col-span-10 overflow-y-auto row-span-11 px-4 pr-8">
+	<SideBar asideClass="col-span-4 lg:col-span-3 xl:col-span-2 max-w-sm row-span-11" />
+	<main class="col-span-8 lg:col-span-9 xl:col-span-10 overflow-y-auto row-span-11 px-4 pr-8">
 		<slot />
 	</main>
 	<Footer footerClass="col-span-full self-end h-1/6" />
